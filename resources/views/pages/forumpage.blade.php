1dcd93bf850474afb8a8ded5f10a92c7fe9609bc
kingisdave <davidfayreal@gmail.com>
2023-02-09 21:56:36 +0100

    updating interface

--- resources/views/pages/forumpage.blade.php
+++ resources/views/pages/forumpage.blade.php
@@ -13,13 +13,20 @@
                                 @foreach ($myClientblogs as $singleBlog)
                                     <div class="col-xl-3 col-lg-3 col-md-3 col-sm-6 col-12">
                                         <div class="card border-0 shadow m-1 myCardStyle">
-                                            <a href="/blogs/{{$singleBlog->id}}" class="text-decoration-none text-reset">
-                                                <img src="/storage/blogs/{{$singleBlog->blogimage}}" alt="{{$singleBlog->blogimage}}" class="card-img-top cardImage" />
+                                            <a href="/blogs/{{$singleBlog->id}}" 
+                                                class="text-decoration-none text-reset">
+                                                <img src="/storage/blogs/{{$singleBlog->blogimage}}" 
+                                                    alt="{{$singleBlog->blogimage}}" class="card-img-top cardImage" />
                                                 <div class="px-2 pt-2 text-center border-bottom">
-                                                    <h6 class="card-title"><strong>{{$singleBlog->blog_title}}</strong></h6>
+                                                    <h6 class="card-title capitalize">
+                                                        <strong>{{Str::limit($singleBlog->blog_title, 50, '...')}}</strong>
+                                                    </h6>
                                                 </div>
                                                 <div class="clearfix mt-2 px-2 pb-2">
-                                                    <p class="float-start namedP"><img src="images/male.jpg" class="rounded-circle me-2" width="20" /> Anonymous reelrlerelreerer</p>
+                                                    <p class="float-start namedP">
+                                                        <img src="images/male.jpg" class="rounded-circle me-2" width="20" /> 
+                                                        Anonymous reelrlerelreerer
+                                                    </p>
                                                     <small class="float-end text-muted smallyText">3 mins</small>
                                                 </div>
                                             </a>
@@ -42,55 +49,7 @@
                                         </a>
                                     </div>    
                                 </div>
-                                <div class="col-xl-3 col-lg-3 col-md-3 col-sm-6 col-12">                      
-                                    <div class="card border-0 shadow m-1 myCardStyle">
-                                        <a href="#" class="text-decoration-none text-reset">
-                                            <img src="images/skyscrapperOne.jpg" class="card-img-top cardImage" alt="skyscrapperOne" >
-                                            <div class="card-body">
-                                                <h5 class="card-title">Card title <small class="float-end text-muted smallyText">3 mins</small></h5>
-                                                <p class="card-text fontMaxSize">
-                                                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusantium rer
-                                                    This is a wider card with supporting text below as a</p>
-                                                <div class="clearfix mt-2">
-                                                    <p class="float-start namedP"><img src="images/male.jpg" class="rounded-circle me-2" width="20" /> Anonymous</p>
-                                                    <a href="#" class="small text-reset float-end text-decoration-none">Read more <i class="fas fa-angle-double-right"></i></a>
-                                                </div>
-                                            </div>
-                                        </a>
-                                    </div>
-                                </div>
-                                <div class="col-xl-3 col-lg-3 col-md-3 col-sm-6 col-12">
-                                    <div class="card border-0 shadow m-1 myCardStyle">
-                                        <a href="#" class="text-decoration-none text-reset">
-                                            <img src="images/skyscrapperOne.jpg" class="card-img-top cardImage" alt="skyscrapperOne" >
-                                            <div class="card-body">
-                                                <h5 class="card-title">Card title <small class="float-end text-muted smallyText">3 mins</small></h5>
-                                                <p class="card-text fontMaxSize">This is a wider card with supporting text below as a</p>
-                                                <div class="clearfix mt-2">
-                                                    <p class="float-start namedP"><img src="images/male.jpg" class="rounded-circle me-2" width="20" /> Anonymous</p>
-                                                    <a href="#" class="small text-reset float-end text-decoration-none">Read more <i class="fas fa-angle-double-right"></i></a>
-                                                </div>
-                                            </div>
-                                        </a>
-                                    </div>
-                                </div>
-                                <div class="col-xl-3 col-lg-3 col-md-3 col-sm-6 col-12">
-                                    <div class="card border-0 shadow m-1 myCardStyle">
-                                        <a href="#" class="text-decoration-none text-reset">
-                                            <img src="images/skyscrapperOne.jpg" class="card-img-top cardImage" alt="skyscrapperOne" >
-                                            <div class="card-body">
-                                                <h5 class="card-title">Card title <small class="mb-3 float-end text-muted smallyText">3 mins</small></h5>
-                                                <p class="card-text fontMaxSize">
-                                                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusantium rer
-                                                    This is a wider card with supporting text below as a</p>
-                                                <div class="clearfix mt-2">
-                                                    <p class="float-start namedP"><img src="images/male.jpg" class="rounded-circle me-2" width="20" /> Anonymous</p>
-                                                    <a href="#" class="small text-reset float-end text-decoration-none">Read more <i class="fas fa-angle-double-right"></i></a>
-                                                </div>
-                                            </div>
-                                        </a>
-                                    </div>    
-                                </div> --}}
+                                 --}}
                                 <div class="d-flex mb-3 mx-4 stepBtns">
                                     <a href="#" class="me-auto btn btn-outline-secondary shadow-lg">Previous</a>
                                     
